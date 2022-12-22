a082420fc682488d86cb9ab5ba225f82c99417f1
Sabiha Iffet Ozkan <sabiha.ozkan@getir.com>
2022-12-22 10:02:44 +0300

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import io from 'socket.io-client'; 
+
+let socket;
 
 function App() {
+  useEffect(() => {
+    socket = io('ws://localhost:5000');
+    socket.on('connection', socket => {
+      console.log("Connection happened")
+    })
+  })
+
   return (
     <div className="App">
       <header className="App-header">
